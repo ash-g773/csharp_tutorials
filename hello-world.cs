ceb2ed9ef0a16e8b0ef835ab2847f7fe9bb78d9f
ash-g773 <aishwaryageorge@ymail.com>
2026-08-27 15:33:41 +0100

    add hello-world file with string tutorials

---
+++ hello-world.cs
@@ -0,0 +1,7 @@
+//Console.WriteLine("Hello, World!");
+
+string aFriend = "Sanju";
+aFriend = "Tresa";
+Console.WriteLine($"Hello {aFriend}");
+
+Console.WriteLine($"Your name is {aFriend.Length} characters long!");
