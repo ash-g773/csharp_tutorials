aa97ca8d9671ca630be255d78dbf5f64a7f5e4ab
ash-g773 <aishwaryageorge@ymail.com>
2026-08-27 16:13:26 +0100

    Finish tuples and types

--- tuples.cs
+++ tuples.cs
@@ -15,4 +15,18 @@ Console.WriteLine($"The point is at {pt2}");
 var subscript = (A: 0, B: 0);
 subscript = pt;
 Console.Write(subscript.A);
-//Console.Write(subscript.X);
+//Console.Write(subscript.X); - this doesnt work
+
+//record - type thats good for storing 2D graph space point values
+Point pt3 = new Point(1, 1);
+var pt4 = pt3 with { Y = 10 };
+Console.WriteLine($"The two points are {pt3} and {pt4}");
+
+double slopeResults = pt4.Slope();
+Console.WriteLine($"The slope of {pt4} is {slopeResults}");
+
+// declaring and using a new type needs to be at the bottom of the file
+public record Point(int X, int Y)
+{
+    public double Slope() => (double)Y / (double)X;
+}
