b077266e102aa484b49e4b07b4419ca47cdd345d
ash-g773 <aishwaryageorge@ymail.com>
2026-08-27 15:48:39 +0100

    finishing string tutorial

--- hello-world.cs
+++ hello-world.cs
@@ -5,3 +5,25 @@ aFriend = "Tresa";
 Console.WriteLine($"Hello {aFriend}");
 
 Console.WriteLine($"Your name is {aFriend.Length} characters long!");
+
+//square brackets just for visualization
+string greeting = "       hey      ";
+Console.Write( $"[{greeting}]" );
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+string sayHello = "Hello, World";
+sayHello = sayHello.Replace("Hello", "Goodbye");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+
+Console.WriteLine(sayHello.Contains("World"));
+Console.WriteLine(sayHello.StartsWith("Goodbye"));
